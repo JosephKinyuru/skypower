0d2bfd1bfe0ebdbc3734330b543c01ca2ee91c28
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-01-21 23:04:45 +0300

    Made blank pages

--- app/(root)/power/page.tsx
+++ app/(root)/power/page.tsx
@@ -1,8 +1,14 @@
-async function Page () {
+function Page () {
     return (
-        <section>
-            <h1 className="heard-text">Power Page</h1>
-        </section>
+    <>
+        <main>
+        <div className='flex items-center justify-center bg-cover bg-center bg-no-repeat bg-[url(https://images.unsplash.com/photo-1473341304170-971dccb5ac1e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] w-full h-screen'>
+                <div className="text-white text-center">
+                <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold mb-4">Power Page</h1>
+                </div>
+            </div>
+        </main>
+    </>
     )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,49 +21,110 @@ const Navbar = () => {
   const router = useRouter();
   const pathname = usePathname();
   const [toggle, setToggle] = useState(false);
+  const toggleMenu = () => setToggle(!toggle);
   
   return (
-    <nav className='sticky w-full mx-auto h-18 shadow-xl bg-white top-0 z-30 flex items-center justify-between px-6 py-3'>
-      <div className="flex items-center">
-        <p className="text-light-1 mr-4">
-        <Image
-          src='skypowerlogo.svg'
-          alt='logo'
-          width={200}
-          height={70}
-          className='object-contain'
-          />
-        </p>
-      </div>
+    <>
+      <nav className='sticky w-full mx-auto h-18 shadow-xl bg-white top-0 z-30 flex items-center justify-between px-6 py-3'>
+        <div className="flex items-center">
+          <p className="text-light-1 mr-4">
+          <Image
+            src='skypowerlogo.svg'
+            alt='logo'
+            width={200}
+            height={70}
+            className='object-contain'
+            />
+          </p>
+        </div>
 
-       <div className="items-center hidden lg:flex">
-            {navbarLinks.map((link, index) => {
+        <div className="items-center hidden lg:flex">
+              {navbarLinks.map((link, index) => {
+              const isActive =
+                  (pathname.includes(link.route) && link.route.length > 1) ||
+                  pathname === link.route;
+
+              return (
+                  <React.Fragment key={index}>
+                  {index > 0 && <span className="text-light-1"> | </span>}
+                  {link.dropdown ? (
+                    <Link
+                    href={link.route}
+                    key={link.label}
+                    className={`gap-4 p-4 ${isActive && "text-yellow-custom"}`}
+                  >
+                  <DropdownMenu>  
+                      <DropdownMenuTrigger>
+                      <p className='text-light-1'>{link.label}</p>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                      {link.dropdown.map((item, index) => (
+                        <DropdownMenuItem key={index}>
+                          {item}
+                        </DropdownMenuItem>
+                      ))}
+                      </DropdownMenuContent>
+                  </DropdownMenu>
+                  </Link>
+                  ) : (
+                    <Link
+                      href={link.route}
+                      key={link.label}
+                      className={`gap-4 p-4 ${isActive && "text-yellow-custom"}`}
+                    >
+                      <p className='text-light-1'>{link.label}</p>
+                    </Link>
+                  )}
+                </React.Fragment>
+              );
+              })}
+          </div>
+
+          <div className="lg:hidden flex flex-1 justify-end items-center">
+            <Image
+              src={toggle ? close : menu}
+              alt="menu"
+              className="w-[28px] h-[28px] object-contain transition-all duration-300 ease-in-out"
+              onClick={() => toggleMenu()}
+            />
+          </div>
+      </nav>
+      {toggle && (
+        <div
+        className={`lg:hidden sticky flex flex-col justify-end items-start ${
+          toggle ? 'transition-transform transform-translate-x-0' : 'transform-translate-x-full'
+        }`}
+        style={{
+          width: 'calc(100vw - 100px)',
+          marginLeft: '20px',
+        }}
+      >          
+           {navbarLinks.map((link, index) => {
             const isActive =
-                (pathname.includes(link.route) && link.route.length > 1) ||
-                pathname === link.route;
+              (pathname.includes(link.route) && link.route.length > 1) ||
+              pathname === link.route;
 
             return (
-                <React.Fragment key={index}>
-                {index > 0 && <span className="text-light-1"> | </span>}
+              <React.Fragment key={index}>
                 {link.dropdown ? (
                   <Link
-                  href={link.route}
-                  key={link.label}
-                  className={`gap-4 p-4 ${isActive && "text-yellow-custom"}`}
-                >
-                <DropdownMenu>  
-                    <DropdownMenuTrigger>
-                    <p className='text-light-1'>{link.label}</p>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                    {link.dropdown.map((item, index) => (
-                      <DropdownMenuItem key={index}>
-                        {item}
-                      </DropdownMenuItem>
-                    ))}
-                    </DropdownMenuContent>
-                </DropdownMenu>
-                </Link>
+                    href={link.route}
+                    key={link.label}
+                    className={` p-4 ${isActive && "text-yellow-custom mb-1"}`}
+                  >
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <p className='text-light-1'>{link.label}</p>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                        {link.dropdown.map((item, index) => (
+                          <DropdownMenuItem key={index}>
+                            {item}
+                          </DropdownMenuItem>
+                        ))}
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </Link>
                 ) : (
                   <Link
                     href={link.route}
@@ -75,30 +136,10 @@ const Navbar = () => {
                 )}
               </React.Fragment>
             );
-            })}
+          })}
         </div>
-
-        <div className="lg:hidden flex flex-1 justify-end items-center">
-          <Image
-            src={toggle ? close : menu}
-            alt="menu"
-            className="w-[28px] h-[28px] object-contain transition-all duration-300 ease-in-out"
-            onClick={() => setToggle(!toggle)}
-          />
-          <aside
-            className={`hidden lg:flex lg:justify-center lg:items-center lg:fixed lg:top-0 lg:bottom-0 lg:right-0 lg:p-50 lg:pt-50 lg:pb-10 lg:w-min-75vw lg:h-screen lg:outline-none lg:bg-white lg:shadow-lg lg:z-9 lg:transition-transform lg:transition-visibility ${
-              toggle ? "lg:translate-x-0 lg:visible" : "lg:translate-x-full lg:hidden"
-            }`}
-            style={{
-              filter: "none",
-              pointerEvents: "auto"
-            }}
-          >
-          </aside>
-        </div>
-
-
-    </nav>
+      )}
+    </>
   )
 }
 
