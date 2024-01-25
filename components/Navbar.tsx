dc1022b626e06d099799ea31ae73f43562b51cf4
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-01-25 15:18:58 +0300

    Mobile navbar fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -51,7 +51,7 @@ const Navbar = () => {
                     <Link
                     href={link.route}
                     key={link.label}
-                    className={`gap-4 p-4 ${isActive && "text-yellow-custom"}`}
+                    className={`gap-4 p-4 ${isActive && "text-yellow-custom"} hover:text-yellow-custom`}
                   >
                   <DropdownMenu>  
                       <DropdownMenuTrigger>
@@ -70,7 +70,7 @@ const Navbar = () => {
                     <Link
                       href={link.route}
                       key={link.label}
-                      className={`gap-4 p-4 ${isActive && "text-yellow-custom"}`}
+                      className={`gap-4 p-4 ${isActive && "text-yellow-custom"} hover:text-yellow-custom`}
                     >
                       <p className='text-light-1'>{link.label}</p>
                     </Link>
@@ -89,56 +89,63 @@ const Navbar = () => {
             />
           </div>
       </nav>
-      {toggle && (
-        <div
-        className={`lg:hidden sticky flex flex-col justify-end items-start ${
-          toggle ? 'transition-transform transform-translate-x-0' : 'transform-translate-x-full'
-        }`}
-        style={{
-          width: 'calc(100vw - 100px)',
-          marginLeft: '20px',
-        }}
-      >          
-           {navbarLinks.map((link, index) => {
-            const isActive =
-              (pathname.includes(link.route) && link.route.length > 1) ||
-              pathname === link.route;
+        {toggle && (
+        <div className={`fixed inset-y-0 z-10 flex w-80 ${toggle ? 'transform translate-x-0' : 'transform -translate-x-full'}`}>        
+          <svg
+          className="absolute inset-0 w-full h-full text-white"
+          style={{ filter: 'drop-shadow(10px 0 10px #00000030)' }}
+          preserveAspectRatio="none"
+          viewBox="0 0 309 800"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+          >
+          <path d="M268.487 0H0V800H247.32C207.957 725 207.975 492.294 268.487 367.647C329 243 314.906 53.4314 268.487 0Z" />
+          </svg>
+            <div 
+            className="z-10 flex flex-col flex-1 text-black"
+            style={{ marginTop: '100px' }}
+            >
+              {navbarLinks.map((link, index) => {
+              const isActive =
+               (pathname.includes(link.route) && link.route.length > 1) ||
+                pathname === link.route;
 
-            return (
-              <React.Fragment key={index}>
-                {link.dropdown ? (
-                  <Link
-                    href={link.route}
-                    key={link.label}
-                    className={` p-4 ${isActive && "text-yellow-custom mb-1"}`}
-                  >
-                    <DropdownMenu>
-                      <DropdownMenuTrigger>
-                        <p className='text-light-1'>{link.label}</p>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent>
-                        {link.dropdown.map((item, index) => (
-                          <DropdownMenuItem key={index}>
-                            {item}
-                          </DropdownMenuItem>
-                        ))}
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </Link>
-                ) : (
-                  <Link
-                    href={link.route}
-                    key={link.label}
-                    className={`gap-4 p-4 ${isActive && "text-yellow-custom"}`}
-                  >
-                    <p className='text-light-1'>{link.label}</p>
-                  </Link>
-                )}
-              </React.Fragment>
-            );
-          })}
-        </div>
-      )}
+                  return (
+                      <React.Fragment key={index}>
+                      {link.dropdown ? (
+                      <Link
+                      href={link.route}
+                      key={link.label}
+                      className={`gap-4 p-4 ${isActive && "text-yellow-custom"}`}
+                      >
+                      <DropdownMenu>  
+                          <DropdownMenuTrigger>
+                            <p className='text-light-1' onClick={toggleMenu}>{link.label}</p>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent>
+                          {link.dropdown.map((item, index) => (
+                              <DropdownMenuItem key={index} onClick={toggleMenu}>
+                              {item}
+                              </DropdownMenuItem>
+                          ))}
+                          </DropdownMenuContent>
+                      </DropdownMenu>
+                      </Link>
+                      ) : (
+                          <Link
+                          href={link.route}
+                          key={link.label}
+                          className={`gap-4 p-4 ${isActive && "text-yellow-custom"}`}
+                          >
+                          <p className='text-light-1' onClick={toggleMenu}>{link.label}</p>
+                          </Link>
+                      )}
+                      </React.Fragment>
+                  );
+                  })}
+              </div>
+          </div>
+        )}
     </>
   )
 }
