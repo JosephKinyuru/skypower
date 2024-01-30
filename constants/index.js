77a83e65172467fe9b00477c6cbfc32b7ae8433f
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-01-30 16:08:09 +0300

    Added more constants

--- constants/index.js
+++ constants/index.js
@@ -98,32 +98,102 @@ export const navbarLinks = [
           link: "https://www.instagram.com/",
         },
         {
-          name: "Dribble",
-          link: "https://www.dribble.com/",
+          name: "LinkedIn",
+          link: "https://www.linkedin.com/",
         },
       ],
     },
   ];
 
-export const homeProducts = [
+import { SlEnergy } from "react-icons/sl";
+import { TbChartGridDots } from "react-icons/tb";
+import { MdSolarPower } from "react-icons/md";
+import { PiCaretDoubleUpBold } from "react-icons/pi";
+import { FaFire } from "react-icons/fa";
+
+export const Products = [
   {
-    image:'renewable.jpg',
+    id:1,
+    icon: SlEnergy,
+    backgroundImage:'https://images.unsplash.com/photo-1487875961445-47a00398c267?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: "Renewable Energy",
+    description: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
   },
   {
-    image:'minigrid.jpg',
+    id:2,
+    icon: TbChartGridDots,
+    backgroundImage:'https://images.unsplash.com/photo-1648316938614-63737d74702a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title:"Minigrids",
+    description: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
   },
   {
-    image:'water-pump.jpg',
+    id:3,
+    icon: MdSolarPower,
+    backgroundImage:'https://images.unsplash.com/photo-1592833159057-6faf163494a9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: "Solar water pumping",
+    description: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
   },
   {
-    image:'e-mobility.jpg',
-    title:"E-Mobility"
+    id:4,
+    icon: PiCaretDoubleUpBold,
+    backgroundImage:'https://images.unsplash.com/photo-1593941707874-ef25b8b4a92b?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title:"E-Mobility",
+    description: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
+
   },
   {
-    image:'stove.jpg',
-    title:"Cooking stoves"
+    id:5,
+    icon: FaFire,
+    backgroundImage:'https://images.unsplash.com/photo-1603048189002-c4aa2128e887?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title:"Cooking stoves",
+    description: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
   }
 ]
+
+import {
+  RxCrop,
+  RxDesktop,
+  RxPencil2,
+  RxReader,
+  RxRocket,
+  RxAccessibility,
+} from "react-icons/rx";
+
+export const ServiceData = [
+  {
+    icon: RxCrop,
+    title: "Development",
+    content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
+    backgroundImage: "https://images.unsplash.com/photo-1435783459217-ee7fe5414abe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    icon: RxPencil2,
+    title: "Branding",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: "https://images.unsplash.com/photo-1435783459217-ee7fe5414abe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    icon: RxDesktop,
+    title: "Design",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: "https://images.unsplash.com/photo-1435783459217-ee7fe5414abe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    icon: RxReader,
+    title: "Seo",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: "https://images.unsplash.com/photo-1435783459217-ee7fe5414abe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    icon: RxAccessibility,
+    title: "Management",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: "https://images.unsplash.com/photo-1435783459217-ee7fe5414abe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    icon: RxRocket,
+    title: "Production",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: "https://images.unsplash.com/photo-1435783459217-ee7fe5414abe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
