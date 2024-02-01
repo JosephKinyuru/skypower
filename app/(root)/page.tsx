54b7d2e02ca71f6bfcb8ae415cea6fca605ae98d
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-02-01 22:13:34 +0300

    Updated homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,61 @@
 import Image from 'next/image'
-import Link from 'next/link'
 import ActiveSlider from '@/components/ActiveSlider'
+import GetInTouch from '@/components/GetInTouch';
+import { robot } from '@/assets';
 
 export default function Home() {
+
+  const word = 'SkyPower'; 
   
   return (
     <>
       <main>
-        <div className='flex items-center justify-center bg-cover bg-center bg-no-repeat bg-[url(https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] w-full h-screen'>
-          <div className="text-slate-50 text-center">
-            <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold mb-4">Delivering Innovative Engineering Solutions that Secure <span className='text-yellow-500'>a bright future for Africa</span></h1>
-            <div className='flex flex-row justify-center space-x-10 pt-[4rem]'>
-              <Link href={'/products'}>
-                <button className='bg-amber-400 px-4 md:px-6 lg:px-8 py-3 md:py-4 lg:py-5 text-base md:text-lg lg:text-2xl tracking-widest border border-amber-300 rounded-xl hover:bg-transparent hover:text-amber-300 hover:border-amber-300 transition duration-300 ease-in-out text-white font-bold'>Our Products</button>              
-              </Link>
-              <Link href={'/solutions'}>
-                <button className='bg-sky-500 px-4 md:px-6 lg:px-8 py-3 md:py-4 lg:py-5 text-base md:text-lg lg:text-2xl tracking-widest border border-sky-500 rounded-xl hover:bg-transparent hover:text-sky-500 hover:border-sky-500 transition duration-300 ease-in-out text-white font-bold'>Our Solutions</button>              
-              </Link>
-            </div>
-          </div>
+        <div className="bg-gradient-to-r from-white via-sky-200 to-sky-80 flex justify-center items-start min-h-screen w-full">
+            <div className="xl:max-w-[1280px] w-full">
+                <section id="home" className='flex md:flex-row flex-col sm:py-16 py-'>
+                  <div className='flex-1 flex justify-center items-start flex-col xl:px-0 sm:px-16 px-6'>
+
+                    <div className="flex flex-row justify-between items-center w-full">
+                      <h1 className="flex-1 font-poppins font-semibold sm:text-[72px] text-[52px] text-black sm:leading-[100.8px] leading-[75px]">
+                          Delivering <br className="sm:block hidden" />{" "}
+                          <span className="text-gradient">Innovative</span>{" "}
+                      </h1>
+                      <div className="ss:flex hidden md:mr-4 mr-0">
+                          <GetInTouch />
+                      </div>
+                      </div>
+
+                      <h1 className="font-poppins font-semibold sm:text-[68px] text-[52px] text-black sm:leading-[100.8px] leading-[75px] w-[120%]">
+                      Engineering Solutions.
+                      </h1>
+                      <p className='font-poppins font-normal text-[#000000] text-[18px] leading-[30.8px] max-w-[470px] mt-5 opacity-70'>
+                      Our team provides smart and effective solutions and services that will seamlessly help your business run optimally, regardless of industry.
+                      </p>
+                    </div>
+
+                        <div className='flex-1 flex justify-center items-center md:my-0 my-10 relative'>
+                            <Image 
+                            src={robot} 
+                            alt="billing" 
+                            className="w-[100%] h-[100%]" 
+
+                            />
+
+                            {/* gradient start */}
+                            <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+                            <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+                            <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+                            {/* gradient end */}
+                        </div>
+
+                        <div className='sm:hidden flex justify-center items-center p-6'>
+                            <GetInTouch />
+                        </div>
+                    </section>
+                </div>
         </div>
-        <div className='flex items-center justify-center bg-cover bg-center bg-no-repeat bg-[url(https://images.unsplash.com/photo-1534298261662-f8fdd25317db?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] w-full h-[130vh] border-t-[5px] border-b-[5px] border-orange-500 py-8'>
+
+        <div className='flex items-center justify-center bg-cover bg-center bg-no-repeat bg-[url(https://images.unsplash.com/photo-1534298261662-f8fdd25317db?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] w-full h-[130vh] border-t-[2px] border-b-[5px] border-orange-500 py-8'>
           <div className="text-center max-w-4xl mx-auto mt-[-20rem]">
               <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-2 mt-0 text-cyan-600 font-serif">WELCOME TO SKYPOWER</h1>
               <h1 className="text-2xl md:text-3xl lg:text-4xl mb-2 pt-10">
