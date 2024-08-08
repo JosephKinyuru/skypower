4f144607f73b7eabba4e21d5b201fe46fa686291
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-08-08 22:01:21 +0300

    Improved navbar dropdowns to be on hover

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,28 +4,31 @@ import React, { useState } from 'react';
 import Link from "next/link";
 import Image from 'next/image';
 import { usePathname} from "next/navigation";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { TfiClose } from "react-icons/tfi";
-
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
+import { motion } from 'framer-motion';
 
 import { navbarLinks } from "@/constants";
+import Menu from './MobileMenu/Menu';
+
+const transition = {
+    type: "spring",
+    mass: 0.5,
+    damping: 11.5,
+    stiffness: 100,
+    restDelta: 0.001,
+    restSpeed: 0.001,
+  };
 
 const Navbar = () => {
   const pathname = usePathname();
-  const [toggle, setToggle] = useState(false);
-  const toggleMenu = () => setToggle(!toggle);
+  const [active, setActive] = useState<string | null>(null);
   
   return (
     <>
-      <nav className='sticky w-full mx-auto h-[4.5rem] shadow-xl bg-zinc-50 top-0 z-30 flex items-center justify-between px-6 py-3'>
-        <div className="flex items-center">
-          <p className="text-light-1 mr-4">
+      <nav 
+        onMouseLeave={() => setActive(null)} // resets the state
+        className='sticky w-full mx-auto h-14 shadow-xl bg-zinc-50 border-b border-gray-200/90 top-0 z-30 flex items-center justify-between px-6 py-3'
+      >
+        <Link href={"/"} className="flex items-center mr-4">
           <Image
             src='skypowerlogo.svg'
             alt='logo'
@@ -33,8 +36,9 @@ const Navbar = () => {
             height={70}
             className='object-contain'
             />
-          </p>
-        </div>
+        </Link>
+
+        <Menu/>
 
         <div className="items-center hidden lg:flex">
               {navbarLinks.map((link, index) => {
@@ -44,115 +48,72 @@ const Navbar = () => {
 
               return (
                   <React.Fragment key={index}>
-                  {index > 0 && <span> | </span>}
+                  {/* {index > 0 &&  <span className="h-[30px] w-px bg-gray-200" aria-hidden="true"/> } */}
+                  {index > 0 &&  <span>|</span> }
                   {link.dropdown ? (
-                    <Link
-                    href={link.route}
-                    key={link.label}
-                    className={`gap-4 p-4 ${isActive && "text-yellow-custom"} hover:text-yellow-custom text-lg`}
-                  >
-                  <DropdownMenu>  
-                      <DropdownMenuTrigger>
-                      <p className='text-light-1'>{link.label}</p>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent>
-                      {link.dropdown.map((item, index) => (
-                        <DropdownMenuItem key={index}>
-                          {item}
-                        </DropdownMenuItem>
-                      ))}
-                      </DropdownMenuContent>
-                  </DropdownMenu>
-                  </Link>
+                    <div
+                        key={link.label}
+                        onMouseEnter={() => setActive(link.label)}
+                        className={`relative gap-4 p-4 ${isActive && "text-yellow-400"} hover:text-yellow-400 cursor-pointer text-black hover:opacity-[0.9]`}
+                    > 
+                        <motion.a 
+                            transition={{ duration: 0.3 }}
+                            className='text-semibold'
+                            href={link.route}
+                        >
+                            {link.label}
+                        </motion.a>
+
+                        {active !== null && (
+                            <motion.div
+                            initial={{ opacity: 0, scale: 0.85, y: 10 }}
+                            animate={{ opacity: 1, scale: 1, y: 0 }}
+                            transition={transition}
+                            >
+                            {active === link.label && (
+                                <div className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4">
+                                <motion.div
+                                    transition={transition}
+                                    layoutId="active" // layoutId ensures smooth animation
+                                    className="bg-white  backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2]  shadow-xl"
+                                >
+                                    <motion.div
+                                    layout // layout ensures smooth animation
+                                    className="w-max h-full p-4"
+                                    >
+                                        <div className="flex flex-col space-y-4 text-sm">
+                                            {link.dropdown.map((item, index) => (
+                                                <Link href={`${item.route}`} key={index} className="text-neutral-700 hover:text-black ">
+                                                    {item.label}
+                                                </Link>
+                                            ))}
+                                        </div>
+                                    </motion.div>
+                                </motion.div>
+                                </div>
+                            )}
+                            </motion.div>
+                        )}
+
+                    </div>
                   ) : (
                     <Link
                       href={link.route}
                       key={link.label}
-                      className={`gap-4 p-4 ${isActive && "text-yellow-custom"} hover:text-yellow-custom text-lg`}
+                      className={`gap-4 p-4 ${isActive && "text-yellow-400"} hover:text-yellow-400`}
                     >
-                      <p className='text-light-1'>{link.label}</p>
+                      <p className='text-semibold'>{link.label}</p>
                     </Link>
                   )}
                 </React.Fragment>
               );
               })}
           </div>
-
-          <div className="lg:hidden flex flex-1 justify-end items-center">
-            {toggle ? (
-              <TfiClose
-                className="w-[28px] h-[28px] text-slate-900 cursor-pointer"
-                onClick={() => toggleMenu()}
-                size={30}
-              />
-            ) : (
-              <RxHamburgerMenu
-                className="w-[28px] h-[28px] text-slate-900 cursor-pointer"
-                onClick={() => toggleMenu()}
-                size={30}
-              />
-            )}
-          </div>
       </nav>
-        {toggle && (
-        <div className={`fixed inset-y-0 z-10 flex w-80 duration-700 ease-in-out`}>        
-          <svg
-          className="absolute inset-0 w-full h-full text-white duration-700 ease-in-out"
-          style={{ filter: 'drop-shadow(10px 0 10px #00000030)' }}
-          preserveAspectRatio="none"
-          viewBox="0 0 309 800"
-          fill="currentColor"
-          xmlns="http://www.w3.org/2000/svg"
-          >
-          <path d="M268.487 0H0V800H247.32C207.957 725 207.975 492.294 268.487 367.647C329 243 314.906 53.4314 268.487 0Z" />
-          </svg>
-            <div 
-            className="z-10 flex flex-col flex-1 text-black"
-            style={{ marginTop: '100px' }}
-            >
-              {navbarLinks.map((link, index) => {
-              const isActive =
-               (pathname.includes(link.route) && link.route.length > 1) ||
-                pathname === link.route;
-
-                  return (
-                      <React.Fragment key={index}>
-                      {link.dropdown ? (
-                      <Link
-                      href={link.route}
-                      key={link.label}
-                      className={`gap-4 p-4 ${isActive && "text-yellow-custom"}`}
-                      >
-                      <DropdownMenu>  
-                          <DropdownMenuTrigger>
-                            <p className='text-light-1' onClick={toggleMenu}>{link.label}</p>
-                          </DropdownMenuTrigger>
-                          <DropdownMenuContent>
-                          {link.dropdown.map((item, index) => (
-                              <DropdownMenuItem key={index} onClick={toggleMenu}>
-                              {item}
-                              </DropdownMenuItem>
-                          ))}
-                          </DropdownMenuContent>
-                      </DropdownMenu>
-                      </Link>
-                      ) : (
-                          <Link
-                          href={link.route}
-                          key={link.label}
-                          className={`gap-4 p-4 ${isActive && "text-yellow-custom"}`}
-                          >
-                          <p className='text-light-1' onClick={toggleMenu}>{link.label}</p>
-                          </Link>
-                      )}
-                      </React.Fragment>
-                  );
-                  })}
-              </div>
-          </div>
-        )}
     </>
   )
 }
 
 export default Navbar
+
+
